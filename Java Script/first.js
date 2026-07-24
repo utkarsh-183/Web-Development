2f28da5988dc1d8a0bc7f5197b464e71cc2312c3
Utkarsh@0011gmail.com <tiwariutkarsh0011@gmail.com>
2026-07-25 00:12:18 +0530

    java script

--- Java Script/first.js
+++ Java Script/first.js
@@ -1,1 +1,52 @@
-console.log('welcome to Java Script by apna college!!');
+// console.log('welcome to Java Script by apna college!!');
+// console.log('I love Mathematics')
+// console.log('currently focussing to complete c++')
+
+// name = "Utkarsh Tiwari"
+// Name = "abhinav"
+// console.log(name);
+// console.log(Name);
+
+// x = null;
+// console.log(x);
+
+// y = undefined;
+// console.log(y);
+
+// isfollow = true;
+// console.log(isfollow);
+
+// let num = 16;
+// console.log(num);
+// console.log(typeof num)
+
+// let name = 'utkarsh';
+// console.log(name);
+// console.log(typeof name);
+
+// isbool = true;
+// console.log(isbool);
+// console.log(typeof isbool);
+
+// const student = {
+//     name : 'Utkarsh',
+//     age : '18',
+//     cgpa : '8.94',
+//     isPass :  true,
+// };
+
+// const product = {
+//     title : "Parker Ball pen",
+//     rating : "4",
+//     offer : 5,
+//     price : 270,
+// };
+
+const profile = {
+    userName : 'Shraddha Khapra',
+    isFollow : true,
+    followers : 569,
+    folloeing : 101,
+};
+
+console.log(typeof profile['followers'])
