b70235c688e0c43e40bcc6ecc2bbd24577cd8aff
Utkarsh@0011gmail.com <tiwariutkarsh0011@gmail.com>
2026-07-25 11:40:19 +0530

    arrays

---
+++ Java Script/array.js
@@ -0,0 +1,34 @@
+// let marks = [91,90,82,84,96];
+// // console.log(marks);
+// // console.log(marks.length);
+// // console.log(typeof(marks));
+
+// for(let i=0; i<marks.length; i++){
+//     console.log(marks[i]);
+// }
+
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+// for(let i=0; i<marks.length; i++){
+//     sum  += marks[i];
+// }
+// console.log(sum/marks.length);
+
+// let price = [250, 645, 300, 900, 50];
+// for(let i=0; i<price.length; i++){
+//     let newPrice = price[i] - (price[i]/10);
+//     console.log(`newPrice is : `,newPrice);  
+// }
+
+// let foodItems = [`Apple`, `Mango`, `Strawberry`, `Guava`];
+// console.log(foodItems);
+// foodItems.push(`litchi`, `Grapes`, `watermelon`);
+// console.log(foodItems);
+// foodItems.pop();
+// console.log(foodItems);
+// console.log(foodItems.toString());
+
+let companies = [`bloomberg`, `microsoft`, `uber`, `rapido`, `google`, `netflix`];
+// companies.shift();
+// companies.splice(2,1,"Ola");
+companies.push(`Amazon`);
