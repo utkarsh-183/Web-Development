4f10439a1ab843487ca2a09ca4d7e1d2704823f9
Utkarsh@0011gmail.com <tiwariutkarsh0011@gmail.com>
2026-08-06 22:40:47 +0530

    Tic Tac Toe

---
+++ projects/Tic Tac Toe/script.js
@@ -0,0 +1,100 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-button");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; 
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+// Click on boxes
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } 
+        else {
+            box.innerText = "X";
+            turnO = true;
+        }
+
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+// Disable all boxes after someone wins
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+// Enable all boxes
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+// Show winner
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+
+    disableBoxes();
+};
+
+// Check winner
+const checkWinner = () => {
+
+    for (let pattern of winPatterns) {
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+
+            if (pos1Val == pos2Val && pos2Val == pos3Val) {
+
+                console.log("Winner", pos1Val);
+
+                showWinner(pos1Val);
+
+                return;
+            }
+        }
+    }
+};
+
+// Reset game
+const resetGame = () => {
+
+    turnO = true;
+
+    enableBoxes();
+
+    msgContainer.classList.add("hide");
+};
+
+// Reset button
+resetBtn.addEventListener("click", resetGame);
+
+// New Game button
+newGameBtn.addEventListener("click", resetGame);
