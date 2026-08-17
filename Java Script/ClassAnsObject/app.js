fd22d9e64f3e22b9858e41c3e2a8b747f61cfa53
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-17 17:34:48 +0530

    object modified

--- Java Script/ClassAnsObject/app.js
+++ Java Script/ClassAnsObject/app.js
@@ -1,7 +1,22 @@
-const student = {
-    fullName : "Utkarsh Tiwari",
-    marks : 91,
-    printMarks : function(){
-        console.log("marks = ",this.marks);
+// const student = {
+//     fullName : "Utkarsh Tiwari",
+//     marks : 91,
+//     printMarks : function(){
+//         console.log("marks = ",this.marks);
+//     },
+// };
+
+const employee = {
+    calcTax() {
+        console.log("tax rate is 10%");
     },
 };
+
+const karanArjun = {
+    salary : 50000,
+    calcTax() {
+        console.log("tax rate is 20%");
+    },
+};
+
+karanArjun.__proto__ = employee;
