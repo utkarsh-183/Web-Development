4b7f45b344da5c3d9d4d8d10219879618daca0ad
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-18 15:40:25 +0530

    syncronous and asyncronous data

---
+++ Java Script/ClassAnsObject/Syncronous Program/app.js
@@ -0,0 +1,18 @@
+console.log("one");
+console.log("two");
+
+setTimeout(() => {
+    console.log("Hello");
+}, 1000);
+
+console.log("three");
+
+// nested loop
+
+// let age = 32;
+// if(age>=18){
+//     if(age>=60) console.log('senior');
+//     else console.log('middle');
+// } else{
+//     console.log('minor');
+// }
