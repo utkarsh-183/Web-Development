3e0851b4bd94a75a9b9d9c6a525aef7bf0bec58d
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-18 15:40:59 +0530

    nested loop

--- Java Script/ClassAnsObject/Syncronous Program/app.js
+++ Java Script/ClassAnsObject/Syncronous Program/app.js
@@ -1,18 +1,17 @@
-console.log("one");
-console.log("two");
+// console.log("one");
+// console.log("two");
 
-setTimeout(() => {
-    console.log("Hello");
-}, 1000);
+// setTimeout(() => {
+//     console.log("Hello");
+// }, 1000);
 
-console.log("three");
+// console.log("three");
 
 // nested loop
-
-// let age = 32;
-// if(age>=18){
-//     if(age>=60) console.log('senior');
-//     else console.log('middle');
-// } else{
-//     console.log('minor');
-// }
+let age = 32;
+if(age>=18){
+    if(age>=60) console.log('senior');
+    else console.log('middle');
+} else{
+    console.log('minor');
+}
