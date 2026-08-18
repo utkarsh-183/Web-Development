c6a6cbccda5bef36a9e2227f2a709ca3bb6b6eef
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-18 15:52:06 +0530

    callback hell

--- Java Script/ClassAnsObject/Syncronous Program/app.js
+++ Java Script/ClassAnsObject/Syncronous Program/app.js
@@ -8,10 +8,32 @@
 // console.log("three");
 
 // nested loop
-let age = 32;
-if(age>=18){
-    if(age>=60) console.log('senior');
-    else console.log('middle');
-} else{
-    console.log('minor');
+// let age = 32;
+// if(age>=18){
+//     if(age>=60) console.log('senior');
+//     else console.log('middle');
+// } else{
+//     console.log('minor');
+// }
+
+
+function getData(dataId, getNextData){
+    setTimeout(() => {
+        console.log("data", dataId);
+        if(getNextData){
+            getNextData();
+        }
+    }, 2000);
 }
+
+// callback hell
+getData(1, () => {
+    console.log("getting data 2........");
+    getData(2, () =>{
+        console.log("getting data 3......");
+        getData(3, () => {
+            console.log("getting data 4.......");
+            getData(4);
+        });
+    });
+});
