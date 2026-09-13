05506d49e7ba7fff53a0cf63af2c45f6b18ab134
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-13 10:31:20 +0530

    Variables in Java Script

---
+++ Js_Hindi/01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "utkarsh@google.com"
+var accountPasswaord = "12345"
+accountCity  = "Varanasi"
+
+// accountId = 2;
+accountEmail = "abc@132"
+accountPasswaord = "12121212"
+accountCity = "pune"
+
+console.table([accountId, accountEmail, accountPasswaord, accountCity])
+
