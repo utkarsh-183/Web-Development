12a749d281c1c27a327cc2f76c50ab8e097340b1
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-13 22:21:24 +0530

    Comaprison Operation

---
+++ Js_Hindi/01_Basics/04_Comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1)
+// console.log("02" > 1);
+
+// console.log(null < 0)
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(1 > null);
+
+
+// Difference B/W == & ===
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+/*note:
+== converts the datatype
+=== do not convert the data type */
