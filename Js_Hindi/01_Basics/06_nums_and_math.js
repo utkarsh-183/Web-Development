5451116933547066c8696d92278f002566f2b818
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-14 23:56:04 +0530

    Mathematical opeartions in JAVA Script

---
+++ Js_Hindi/01_Basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++MATHS+++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(8.3));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(4, 3, 6, 43));
+// console.log(Math.max(4, 3, 6, 43));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
---
+++ Js_Hindi/01_Basics/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+console.log(Math);
+console.log(Math.abs(-1));
+console.log(Math.round(4.3));
+console.log(Math.ceil(8.3));
+console.log(Math.floor(7.9));
+console.log(Math.min(4, 3, 6, 43));
+console.log(Math.max(4, 3, 6, 43));
